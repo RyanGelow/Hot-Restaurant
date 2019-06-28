e178571578c51dc8e4fec508e270a225520a6ca3
Garrett Young <garrettyoung510@gmail.com>
2019-06-28 10:23:57 -0700

    sql file

---
+++ restaurant.sql
@@ -0,0 +1,27 @@
+-- if making any changes this will drop it
+DROP DATABASE IF EXISTS restaurant_db;
+
+-- create a database called restaurant
+CREATE DATABASE restaurant_db;
+
+USE restaurant_db;
+
+-- create a table inside the database called tables
+CREATE TABLE tables (
+
+-- id unique id for each table
+    id INT NOT NULL AUTO_INCREMENT,
+
+-- reservation name
+reservationName VARCHAR(45) NOT NULL,
+
+-- phone number 
+phoneNumber INT(10) NOT NULL,
+
+-- primary key
+    PRIMARY KEY (id)
+);
+
+-- add 10 different products
+INSERT INTO tables (reservationName, phoneNumber)
+VALUES("test", 911),
